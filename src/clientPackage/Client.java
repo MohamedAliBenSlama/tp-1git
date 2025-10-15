add539971b1d255eda36a4e0e012fdde391b84d9
mohamed ali ben slama <mohamedalibenslama0123@gmail.com>
2025-10-15 16:01:50 +0100

    seconde change

--- src/clientPackage/Client.java
+++ src/clientPackage/Client.java
@@ -1,18 +1,42 @@
 package clientPackage;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Client : en attente de connexion...");
+            Socket socket = new Socket("localhost", 1234);
+            System.out.println("Client connecté au serveur !");
 
-	public static void main(String[] args) throws UnknownHostException, IOException{
-		// TODO Auto-generated method stub
-		System.out.println("je suis un client pas encore connectee");
-		Socket socket=new Socket("localhost",1234);
-		System.out.println("je suis un client connectee");
-		socket.close();
+            // Création des flux
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            Scanner scanner = new Scanner(System.in);
 
-	}
+            // Lecture de l’entier saisi par l’utilisateur
+            System.out.print("Entrez un entier x : ");
+            int x = scanner.nextInt();
 
+            // Envoi de l’entier au serveur
+            out.writeInt(x);
+            System.out.println("Client a envoyé x = " + x);
+
+            // Réception du résultat
+            int resultat = in.readInt();
+            System.out.println("Résultat reçu du serveur : " + resultat);
+
+            // Fermeture
+            in.close();
+            out.close();
+            socket.close();
+            scanner.close();
+            System.out.println("Client terminé.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/serverPackage/Server.java
+++ src/serverPackage/Server.java
@@ -1,24 +1,45 @@
 package serverPackage;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    public static void main(String[] args) {
+        try {
+            // Création du serveur sur le port 1234
+            ServerSocket socketServer = new ServerSocket(1234);
+            System.out.println("Serveur démarré, en attente d’un client...");
 
-	public static void main(String[] args) {
-		ServerSocket socketServer;
-		try {
-			socketServer = new ServerSocket(1234);
-		
-		System.out.println("je suis un client pas encore connectee");
-		Socket socket=socketServer.accept();
-		System.out.println("je suis un client connectee");
-		socket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+            // Attente de la connexion d’un client
+            Socket socket = socketServer.accept();
+            System.out.println("Un client est connecté !");
+
+            // Création des flux d’entrée/sortie
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            // Lecture de l’entier envoyé par le client
+            int x = in.readInt();
+            System.out.println("Serveur a reçu x = " + x);
+
+            // Traitement : produit par 5
+            int resultat = x * 5;
+            System.out.println("Serveur a calculé : " + x + " * 5 = " + resultat);
+
+            // Envoi du résultat au client
+            out.writeInt(resultat);
+            System.out.println("Résultat envoyé au client.");
+
+            // Fermeture
+            in.close();
+            out.close();
+            socket.close();
+            socketServer.close();
+            System.out.println("Serveur arrêté.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
